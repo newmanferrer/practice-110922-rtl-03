4b34bcf74a2fd524d21ceb934aea838e41c62600
Newman Ferrer <newmanferrer@gmail.com>
2022-09-13 13:14:35 -0400

    refactor: :zap: Add isLoading and Error state
    Add isLoading and Error state

--- src/hooks/useAPI/useAPI.ts
+++ src/hooks/useAPI/useAPI.ts
@@ -1,11 +1,26 @@
 import { useState, useEffect } from 'react';
 import { IUser } from '../../models';
 
+interface IError {
+  hasError: boolean;
+  message: string;
+}
+
+const errorInitialState: IError = {
+  hasError: false,
+  message: ''
+};
+
 export function useAPI() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(errorInitialState);
   const [data, setData] = useState<IUser[]>([]);
 
   useEffect(() => {
     let isMounted = true;
+    setError(errorInitialState);
+    setIsLoading(true);
+
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => {
         if (!response.ok) throw new Error(`Error code: ${response.status}`);
@@ -16,12 +31,20 @@ export function useAPI() {
           setData(data);
         }
       })
-      .catch(error => console.error(error));
+      .catch(error => handlerError(error))
+      .finally(() => setIsLoading(false));
 
     return () => {
       isMounted = false;
     };
   }, []);
 
-  return { data };
+  const handlerError = (error: any) => {
+    setError({
+      hasError: true,
+      message: error.message
+    });
+  };
+
+  return { data, isLoading, error };
 }
